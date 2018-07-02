50f3cf19e76b4b6aff3c40521ea12e71c504cddc
G0NM <eric.barrier@bcbsfl.com>
2018-07-02 11:58:31 -0400

    updated App with router buttons to switch between demo apps. made App.test more absolute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import ClickCounter from './demos/click-counter/ClickCounter';
 import Jotto from './demos/jotto/Jotto';
 
@@ -11,6 +11,14 @@ class App extends Component
     render()
     {
         return <div className="src-app--container">
+
+            <div className="src-app--route-links">
+
+                <Link to="/counter">Counter</Link>
+
+                <Link to="/jotto">Jotto</Link>
+
+            </div>
 
             <Route path="/counter" render={() => <ClickCounter/>}/>
 
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { findByClassName } from '../utils/TestUtils';
 import App from '../App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -8,4 +9,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test('renders without crashing', () =>
 {
     const wrapper = shallow(<App/>);
+
+    const app = findByClassName(wrapper, '.src-app--container');
+
+    expect(app.length).toBe(1);
 });
